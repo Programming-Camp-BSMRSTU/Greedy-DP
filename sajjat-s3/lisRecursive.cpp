d8956ebbd650c16a49ca5d55e9ab1b916cb55345
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-04-29 09:37:43 +0600

    sajjat

---
+++ sajjat-s3/lisRecursive.cpp
@@ -0,0 +1,102 @@
+/*---------------------------------------
+   BISMILLAHIR RAHMANIR RAHIM
+   AUTHOR : Md. Sajjat Hossen
+   TIME : 29-April,2021 09:28:40 AM
+----------------------------------------*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef  long long               ll;
+typedef  long double             ld;
+typedef  unsigned long long      ull;
+typedef  pair<int, int>          pii;
+typedef  pair<ll, ll>            pll;
+typedef  vector<int>             vi;
+typedef  vector<ll>              vll;
+typedef  tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef  tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
+
+const int N       = (int) 1e3 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const double EPS  = (double) 1e-9;
+
+#define    debug(x)    cerr << #x << " = " << x << '\n';
+#define    all(x)      x.begin(), x.end()
+#define    szof(x)     (int) x.size()
+#define    ff          first
+#define    ss          second
+#define    pb          push_back
+#define    mp          make_pair
+#define    PI          acos(-1.0)
+#define    TN          typename
+#define    nl          '\n'
+#define    Fast_IO     ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+inline int add(int a, int b, int mod) { a += b; return a >= mod ? a - mod : a < 0 ? a + mod : a; }
+inline int mult(int a, int b, int mod) { return (ll) a * b % mod; }
+
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
+
+int ar[N], dp[N], par[N], n;
+
+int fun(int at) {
+    if (dp[at] != -1) return dp[at];
+    int res = 0;
+    for (int i = at + 1; i <= n; ++i) {
+        if (ar[i] > ar[at]) {
+            int sub = fun(i);
+            if (sub > res) {
+                res = sub;
+                par[at] = i;
+            }
+        }
+    }
+    return dp[at] = res + 1;
+}
+
+void path(int at) {
+    if (par[at] != -1) {
+        printf("%d ", ar[at]);
+        path(par[at]);
+    } else printf("%d\n", ar[at]);
+}
+
+int main() {
+    // Fast_IO
+    // clock_t tStart = clock();
+    //freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int test = 1, tc = 0;
+    while (test--) {
+        memset(dp, -1, sizeof dp);
+        memset(par, -1, sizeof par);
+        n = Int();
+        for (int i = 1; i <= n; ++i) ar[i] = Int();
+        int res = 0, start = 1;
+        for (int i = 1; i <= n; ++i) {
+            int sub = fun(i);
+            if (sub > res) {
+                res = sub;
+                start = i;
+            }
+        }
+        printf("Length %d\n", res);
+        printf("Path = ");
+        path(start);
+    }
+    // fprintf(stderr, "\nRuntime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    return 0;
+}

4ce77487113f0f5076621e0dd5a10c20abe6fcfa
shuvo-14 <66715106+shuvo-14@users.noreply.github.com>
2021-04-30 13:28:25 +0600

    Add files via upload

---
+++ shuvo_s03/Coin_change_dp01_iterative_printing_coins.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+const int mod=1e9+7;
+const int N=1e5+9;
+
+void solve()
+{
+    ll n, target;
+    cin >> n >> target;
+    vector<ll>a;
+    for(int i  = 0; i < n; i++){ ll x; cin >> x; a.pb(x);}
+    ll dp[N];
+    dp[0] = 0;
+    ll next[N];
+    for(int x = 1; x <= target; x++)
+    {
+        dp[x] = INT_MAX;
+        for(auto c: a)
+        {
+            if(c <= x && dp[x-c]+1 < dp[x])
+            {
+                dp[x] = dp[x-c] + 1;
+                next[x] = c;
+            }
+        }
+    }
+    cout << dp[target] << endl;
+    ll x = target;
+    while(x)
+    {
+        cout << next[x] << " ";
+        x -= next[x];
+    }
+}
+
+int main()
+{
+    IOS;
+    int t = 1;
+  //  cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
